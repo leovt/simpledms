4128178b4b9acf38b80425c8cff28056613a30cd
leovt <leonhard.vogt@gmx.ch>
2022-03-28 09:31:08 +0000

    added document model

--- src/sdms/models.py
+++ src/sdms/models.py
@@ -1,3 +1,22 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=30)
+    subtag = models.CharField(max_length=30)
+    fill_color = models.CharField(max_length=7, default="#AFD9F3")
+    text_color = models.CharField(max_length=7, default="#233038")
+
+    def __str__(self):
+        if self.subtag:
+            return f'{self.name}: {self.subtag}'
+        return self.name
+
+class Document(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    file = models.FileField()
+    subject = models.CharField(max_length=200)
+    tags = models.ManyToManyField(Tag, related_name='documents')
--- src/simpledms/settings.py
+++ src/simpledms/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sdms.apps.SdmsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
