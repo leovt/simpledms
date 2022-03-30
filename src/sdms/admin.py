67e89315e906e1b7974b40a88f004394fa0bb4a9
leovt <leonhard.vogt@gmx.ch>
2022-03-30 18:33:09 +0000

    color widgets for tags

--- src/sdms/admin.py
+++ src/sdms/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+from django.forms.widgets import TextInput
 
 # Register your models here.
 from .models import Tag, Document
 
-admin.site.register(Tag)
 admin.site.register(Document)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    def get_form(self, request, obj=None, **kwargs):
+        print(kwargs)
+        kwargs['widgets'] = {
+            'text_color': TextInput(attrs={'type': 'color'}),
+            'fill_color': TextInput(attrs={'type': 'color'}),
+        }
+        return super().get_form(request, obj, **kwargs)
