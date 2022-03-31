abe840acfe8a11902becc06f2fbc69bef0d902de
leovt <leonhard.vogt@gmx.ch>
2022-03-31 20:00:26 +0000

    added fields to document

--- src/sdms/models.py
+++ src/sdms/models.py
@@ -2,6 +2,10 @@ import uuid
 
 from django.db import models
 
+import pdftotext
+import pytesseract
+import pdf2image
+
 # Create your models here.
 class Tag(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -18,5 +22,37 @@ class Tag(models.Model):
 class Document(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     file = models.FileField()
-    subject = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Status(models.TextChoices):
+        UNTREATED = 'UT', 'Untreated'
+        INBOX = 'IN', 'Inbox'
+        HOLDFILE = 'HF', 'Hold File'
+        ARCHIVE = 'AR', 'Archive'
+
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.UNTREATED)
+
+    subject = models.CharField(max_length=200, blank=True)
     tags = models.ManyToManyField(Tag, related_name='documents')
+    document_date = models.DateField(null=True, blank=True)
+    document_amount = models.DecimalField(max_digits=19, decimal_places=4, null=True, blank=True)
+
+    pdf_text = models.TextField(blank=True, editable=False)
+    ocr_text = models.TextField(blank=True, editable=False)
+    pages = models.IntegerField(null=True, editable=False)
+
+    def prepare(self):
+        if self.status != Document.Status.UNTREATED:
+            return
+
+        with open(self.file.path, "rb") as f:
+            pdf = pdftotext.PDF(f)
+            self.pages = len(pdf)
+            self.pdf_text = '\n\n'.join(pdf)
+
+        images = pdf2image.convert_from_path(self.file.path, dpi=300)
+        print(images)
+        self.ocr_text = '\n\n'.join(pytesseract.image_to_string(image, 'deu') for image in images)
+
+        self.status = Document.Status.INBOX
+        self.save()
--- src/sdms/views.py
+++ src/sdms/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.db.models import Count
+
 from .models import Document, Tag
+
 
 import pdf2image
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, this is the sdms index.")
+    count = {'UT': 0, 'IN': 0, 'HF': 0, 'AR': 0}
+    queryset = Document.objects.all().values('status').annotate(count=Count('status'))
+    count.update({x['status']: x['count'] for x in queryset})
+
+    print(count)
+
+    return render(request, 'sdms/index.html', count)
 
 def document(request, document_id):
     document = get_object_or_404(Document, id=document_id)
