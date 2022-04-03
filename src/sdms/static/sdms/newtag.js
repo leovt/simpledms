894d3dac60739858b57a0ed6cb2f60bf12a7dbbc
leovt <leonhard.vogt@gmx.ch>
2022-04-03 00:13:24 +0000

    fix new tags

--- src/sdms/static/sdms/newtag.js
+++ src/sdms/static/sdms/newtag.js
@@ -14,9 +14,29 @@
       label.setAttribute('for', id_for_label);
       label.setAttribute('class', 'tag newly-added');
       label.style.color = data.text_color;
-      label.backgroundColor = data.fill_color;
-      label.borderColor = data.border_color;
+      label.style.backgroundColor = data.fill_color;
+      label.style.borderColor = data.border_color;
       parent.appendChild(label);
+
+      parent = document.getElementById("doctags");
+      var input = document.createElement("input");
+      input.setAttribute('class', 'cbtag newly-added');
+      input.setAttribute('id', id_for_label);
+      input.setAttribute('name', 'tags');
+      input.setAttribute('type', 'checkbox');
+      input.setAttribute('value', data.tag_id);
+      input.setAttribute('checked', '');
+      parent.appendChild(input);
+      label = document.createElement("label");
+      label.innerText = data.label;
+      label.setAttribute('for', id_for_label);
+      label.setAttribute('class', 'tag newly-added');
+      label.style.color = data.text_color;
+      label.style.backgroundColor = data.fill_color;
+      label.style.borderColor = data.border_color;
+      parent.appendChild(label);
+
+
     };
     request.open("POST", form.action);
     request.send(new FormData(form));
