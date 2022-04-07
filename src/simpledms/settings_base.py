b1f1c6f5371766c09bca95b51b706ad7e4d1b5a6
leovt <leonhard.vogt@gmx.ch>
2022-04-07 19:41:09 +0000

    rename settings for development

--- src/simpledms/settings_base.py
+++ src/simpledms/settings_base.py
@@ -15,19 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6hi*eh16npn7rl#e*$h_9&1$ntcu*iq#95-)4ol1!qqs_@x5rt'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
